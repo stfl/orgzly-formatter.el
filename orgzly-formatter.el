6bcc16ed72b7eaad3429047be0137330ed996a7c
Stefan Lendl <git@stfl.dev>
2026-05-17 03:35:49 +0200

    chore: Bump version to 0.3.0

--- orgzly-formatter.el
+++ orgzly-formatter.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2026 Stefan Lendl
 
 ;; Author: Stefan Lendl <git@stfl.dev>
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: outlines, convenience, files
 ;; Homepage: https://github.com/stfl/orgzly-formatter
